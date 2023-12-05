2b71ce17fdbf4e6a89b48f25500d73921ae51275
Omobolaji Arigbede <arigbedeomobolaji@gmail.com>
2023-12-05 09:54:06 +0100

    done with react router checkpoint

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import RootLayout from "./pages/layout/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import MovieDetails from "./pages/MovieDetails";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/movies/:movieId",
+				element: <MovieDetails />,
+			},
+		],
+	},
+]);
+export default function App() {
+	return <RouterProvider router={router}></RouterProvider>;
+}
